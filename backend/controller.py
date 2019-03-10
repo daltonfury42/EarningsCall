3423e3574b3d9a08c3b2c62cb9ed6a7c04fd1152
Dalton Fury <daltonfury42@disroot.org>
2019-03-10 13:46:40 +0530

    Fetching transcript and timings from backend

--- backend/controller.py
+++ backend/controller.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
+import os
+import csv
+
 app = Flask(__name__)
 
-@app.route('/call/<string:call_id>')
-def hello_world(call_id):
-    return render_template('result.html', call_id=call_id)
+@app.route('/call/<string:callId>')
+def results(callId):
+    timeData, texts = getData(callId)
+    return render_template('result.html', callId=callId, timeData=timeData, texts=texts)
 
+
+def getData(callId):
+    transcriptFilePath = os.path.join('data/transcripts', callId + '.csv')
+
+    timeData, texts = [], []
+
+    with open(transcriptFilePath) as csvFile:
+        spamreader = csv.reader(csvFile)
+        for row in spamreader:
+            speaker, text, splitId, startTime, endTime = row[0], row[1], row[2], row[3], row[4]
+            text = text.decode('utf-8')
+            timeData.append({'splitId': splitId, 'startTime': startTime, 'endTime': endTime})
+            texts.append({'splitId': splitId, 'speaker': speaker, 'text': text})
+
+    return  timeData, texts
