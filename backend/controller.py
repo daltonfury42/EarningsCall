ddd468e4d36e797c9d69a1e98925e03845704add
Dalton Fury <daltonfury42@disroot.org>
2019-03-14 12:23:06 +0530

    Fixed results title

--- backend/controller.py
+++ backend/controller.py
@@ -4,13 +4,14 @@ import random
 from flask import Flask, render_template
 import os
 import csv
+import re
 
 app = Flask(__name__)
 
 @app.route('/call/<string:callId>')
 def results(callId):
-    timeData, texts = getData(callId)
-    return render_template('result.html', callId=callId, timeData=timeData, texts=texts)
+    title, timeData, texts = getData(callId)
+    return render_template('result.html', callId=callId, timeData=timeData, texts=texts, title=title)
 
 @app.route('/call/')
 def calls():
@@ -36,16 +37,23 @@ def getData(callId):
     transcriptFilePath = os.path.join('data/transcripts', callId + '*.csv')
     transcriptFilePath = glob.glob(transcriptFilePath)[0]
 
+    base, _ = os.path.splitext(transcriptFilePath)
+    header = base.split('-')[1:]
+    header = ' '.join(header).title()
+
+    header = re.sub('Earnings Call Transcript$', '', header)
+
+    title = re.sub(r'Ceo[ A-Za-z]*', 'Q', header)
+
     timeData, texts = [], []
 
     with open(transcriptFilePath) as csvFile:
         spamreader = csv.reader(csvFile)
         for row in spamreader:
-            print(row)
             splitId, startTime, endTime, _, speaker, text = row[0], row[1], row[2], row[3], row[4], row[5]
             text = text.decode('utf-8')
             timeData.append({'splitId': splitId, 'startTime': startTime, 'endTime': endTime})
             texts.append({'splitId': splitId, 'speaker': speaker, 'text': text,
                           'emotion': random.choice(['happy', 'sad', 'neutral'])})
 
-    return  timeData, texts
+    return  title, timeData, texts
