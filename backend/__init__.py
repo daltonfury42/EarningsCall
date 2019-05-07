769b97101c167a224eab1b342035bad3b5fa7c3b
Dalton Fury <daltonfury42@disroot.org>
2019-05-08 01:06:11 +0530

    Fix

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 @app.route('/call/<string:callId>')
 def results(callId):
     title, timeData, texts, emotionCount, topicCount, highlights_dict, highlights_flat, tags_dict = service.getData(callId)
-    print(tags_dict)
     return render_template('result.html', callId=callId, timeData=timeData,
                            texts=texts, title=title, emotionCount=emotionCount,
                            topicCount=topicCount, highlights_dict=highlights_dict,
