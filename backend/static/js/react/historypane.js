53367441de9755d2ea82a60fa19ca9ff9738b6a8
Dalton Fury <daltonfury42@disroot.org>
2019-07-29 09:04:47 +0530

    Removed react list key warning

--- backend/static/js/react/historypane.js
+++ backend/static/js/react/historypane.js
@@ -30,7 +30,10 @@ class HistoryPane extends React.Component {
         var i = 1;
         while(true) {
             const splitId = generateSplitId(i);
-            const row = <HistoryRow dataPoint={this.dataJson[splitId]} onClick={() => this.props.onRowClick(splitId)}/>
+            const row = <HistoryRow 
+                            key={splitId}
+                            dataPoint={this.dataJson[splitId]} onClick={() => this.props.onRowClick(splitId)}
+                        />
             rows.push(row);
 
             if (splitId === this.props.currentSplitId) {
